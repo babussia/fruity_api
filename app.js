eb2dd3db905d13095c9fbeaac75a7302e0d76b78
Olena Tabunshchyk <im.elenaaaa@gmail.com>
2025-04-08 11:18:58 +0100

    udate cors position

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const fruits = require('./routes/fruits');
 
 // Middleware
+app.use(cors());
 app.use(express.json()); 
 
 // Routes
